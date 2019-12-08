24ee8bdb5ac8eb082ce2e179dee58f842308cb9b
Nkuma <nknichols1@icloud.com>
2019-12-08 13:51:25 -0500

    Writing a simple function in python

---
+++ Simply_Function.py
@@ -0,0 +1,8 @@
+# Write a function called loading_screen that prints
+# "This page is loading..." to the console.
+
+def loading_screen():
+  print("This page is loading...")
+  
+loading_screen()
+
