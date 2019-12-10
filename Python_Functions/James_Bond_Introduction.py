b152b693556fc000baa34a80e0d4b0139c93e676
Nkuma <nknichols1@icloud.com>
2019-12-10 11:03:40 -0500

    James Bond introduction using python

---
+++ Python_Functions/James_Bond_Introduction.py
@@ -0,0 +1,8 @@
+#Write a function named introduction() that has two parameters
+#named first_name and last_name.
+
+def introduction(first_name, last_name):
+  return last_name + ", " + first_name + " " + last_name
+
+print(introduction("James", "Bond"))
+# should print Bond, James Bond
