5ee5615c045e3c1a5be04d5058ba1707ccf7407a
Nkuma <nknichols1@icloud.com>
2019-12-10 10:04:36 -0500

    Calculating a function raised to the tenth power using python

---
+++ Python_Functions/Tenth_Power_Function.py
@@ -0,0 +1,14 @@
+# Write a function named tenth_power() that has one parameter named num.
+#The function should return num raised to the 10th power.
+
+
+def tenth_power(num):
+  return num ** 10
+
+
+print(tenth_power(1))
+# 1 to the 10th power is 1
+print(tenth_power(0))
+# 0 to the 10th power is 0
+print(tenth_power(2))
+# 2 to the 10th power is 1024
