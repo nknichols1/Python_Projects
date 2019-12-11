667d88e076334c9bfbe31448f2d94f9a40abe269
Nkuma <nknichols1@icloud.com>
2019-12-11 12:51:06 -0500

    Finding the square of a function using python

---
+++ Python_Functions/Squareroot_Function.py
@@ -0,0 +1,11 @@
+#Write a function named square_root() that has one parameter named num.
+# square root num
+
+def square_root(num):
+  return num ** 0.5
+
+
+print(square_root(16))
+# should print 4
+print(square_root(100))
+# should print 10
